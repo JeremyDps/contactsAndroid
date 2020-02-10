a5e1b23afffdfe61d4cdcdd8b0ea258976664193
Jérémy Dupuis <jeremy.dupuis10@gmail.com>
2020-02-10 11:26:26 +0100

    affichage details contact

--- app/src/main/java/com/example/mescontacts/DetailsContactActivity.java
+++ app/src/main/java/com/example/mescontacts/DetailsContactActivity.java
@@ -2,13 +2,47 @@ package com.example.mescontacts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class DetailsContactActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_contact);
+
+        Intent intent = getIntent();
+
+        if(intent != null) {
+            if(intent.hasExtra("nom")) {
+                String nom = intent.getStringExtra("nom");
+                TextView nom_data = findViewById(R.id.name_data);
+                nom_data.setText(nom);
+            }
+            if(intent.hasExtra("prenom")) {
+                TextView prenom_data = findViewById(R.id.surname_data);
+                String prenom = intent.getStringExtra("prenom");
+                prenom_data.setText(prenom);
+            }
+            if(intent.hasExtra("numero")) {
+                TextView numero_data = findViewById(R.id.number_data);
+                String numero = intent.getStringExtra("numero");
+                numero_data.setText(numero);
+            }
+            if(intent.hasExtra("mail")) {
+                TextView mail_data = findViewById(R.id.mail_data);
+                String mail = intent.getStringExtra("mail");
+                mail_data.setText(mail);
+            }
+            if(intent.hasExtra("adresse")) {
+                TextView adresse_data = findViewById(R.id.adress_data);
+                String adresse = intent.getStringExtra("adresse");
+                adresse_data.setText(adresse);
+            }
+        }
     }
 }
