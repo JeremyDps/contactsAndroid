b57b9bb9bec9d6096abac0aad52983e97a786e93
Jérémy Dupuis <jeremy.dupuis10@gmail.com>
2020-02-13 16:52:04 +0100

    projet en cours

--- app/src/main/java/com/example/mescontacts/DetailsContactActivity.java
+++ app/src/main/java/com/example/mescontacts/DetailsContactActivity.java
@@ -3,46 +3,15 @@ package com.example.mescontacts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailsContactActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_contact);
-
-        ImageView appel = findViewById(R.id.appel);
-        final TextView num_data = findViewById(R.id.number_data);
-
-        ImageView email = findViewById(R.id.mail);
-        TextView mail_data = findViewById(R.id.mail_data);
-
-        appel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("DEBUG", num_data.getText().toString());
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:" + num_data.getText().toString()));
-                startActivity(callIntent);
-            }
-        });
-
-        email.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("DEBUG", num_data.getText().toString());
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:" + num_data.getText().toString()));
-                startActivity(callIntent);
-            }
-        });
 
         Intent intent = getIntent();
 
@@ -53,17 +22,17 @@ public class DetailsContactActivity extends AppCompatActivity {
                 nom_data.setText(nom);
             }
             if(intent.hasExtra("prenom")) {
-                TextView prenom_data = findViewById(R.id.surname_data);
+                TextView prenom_data = findViewById(R.id.firstname_data);
                 String prenom = intent.getStringExtra("prenom");
                 prenom_data.setText(prenom);
             }
             if(intent.hasExtra("numero")) {
-                TextView numero_data = findViewById(R.id.number_data);
+                TextView numero_data = findViewById(R.id.phone_number_data);
                 String numero = intent.getStringExtra("numero");
                 numero_data.setText(numero);
             }
             if(intent.hasExtra("mail")) {
-                TextView mail_data = findViewById(R.id.mail_data);
+                TextView mail_data = findViewById(R.id.email_data);
                 String mail = intent.getStringExtra("mail");
                 mail_data.setText(mail);
             }
--- app/src/main/java/com/example/mescontacts/MainActivity.java
+++ app/src/main/java/com/example/mescontacts/MainActivity.java
@@ -212,7 +212,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     startActivity(Intent.createChooser(emailIntent, "Send mail..."));
                     finish();
-                    Log.i("Finished sending email..", "");
+                    Log.i("Finished sending email", "");
                 } catch (android.content.ActivityNotFoundException ex) {
                     Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
                 }
